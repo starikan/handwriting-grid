8ce72c4580980874e200ed309c58d97480de7345
sstarodubov <s.starodubov@litebox.ru>
2019-09-15 21:36:22 +0400

    Routing

---
+++ index.js
@@ -0,0 +1,20 @@
+// const puppeteer = require ('puppeteer');
+// const express = require('express');
+// var app = express(exports);
+// const browser = await puppeteer.launch();
+
+// const main = async () => {
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:998877');
+
+//     const pdf = await page.pdf();
+//     return pdf;
+//  }
+
+// app.get('/get_pdf', async function (req, res) {
+//   const pdf = await main();
+//   res.contentType("application/pdf");
+//   res.send(pdf);
+// });
+
+// app.listen(3000, function(){ console.log('Listening on 3000') });
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+var router = new VueRouter({
+  mode: 'history',
+  routes: []
+});
+
 var app = new Vue({
+  router,
   el: '#app',
+  mounted: function() {
+    // console.log(this.$route.query.kanji.split(''))
+    this.kanjis = _.get(this.$route.query, 'kanjis', '').split('');
+    console.log(this)
+  },
   computed: {
     kanzisGrid: function() {
-      return this.kanzis.flatMap(v => v.repeat(this.columns).split(''));
+      return this.kanjis.flatMap(v => v.repeat(this.columns).split('')) || [];
     }
   },
   data: {
-    kanzis: ['我', '晚', '電'],
+    kanjis: [],
     columns: 7,
   }
 })
