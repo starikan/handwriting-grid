d7e6c4bd0e41ab53733af22daaff23c8ee0b83fe
sstarodubov <s.starodubov@litebox.ru>
2019-09-16 12:20:00 +0400

    Settings of typography

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 var router = new VueRouter({
   mode: 'history',
-  routes: []
+  routes: [],
 });
 
 var app = new Vue({
   router,
   el: '#app',
   mounted: function() {
-    this.kanjis = _.get(this.$route.query, 'kanjis', '').split('');
-    this.columns = _.get(this.$route.query, 'columns', 7);
-    console.log(this)
+    const initValues = {
+      kanjis: '@',
+      columns: 7,
+      cellSize: 2.5,
+      fontSize: 2.5,
+    };
+
+    this.kanjis = _.get(this.$route.query, 'kanjis', initValues.kanjis);
+    this.columns = _.get(this.$route.query, 'columns', initValues.columns);
+    this.cellSize = _.get(this.$route.query, 'cellSize', initValues.cellSize);
+    this.fontSize = _.get(this.$route.query, 'fontSize', initValues.fontSize);
+    console.log(this);
   },
   computed: {
     kanzisGrid: function() {
-      return this.kanjis.flatMap(v => v.repeat(this.columns).split('')) || [];
-    }
+      return this.kanjis.split('').flatMap(v => v.repeat(this.columns).split('')) || [];
+    },
+  },
+  watch: {
+    cellSize: function(curr, prev) {
+      const cellSize = parseFloat(curr);
+      document.documentElement.style.setProperty('--cell-size', `${cellSize}cm`);
+    },
+    fontSize: function(curr, prev) {
+      const fontSize = parseFloat(curr);
+      document.documentElement.style.setProperty('--font-size', `${fontSize}cm`);
+    },
   },
   data: {
-    kanjis: [],
-    columns: 7,
-  }
-})
+    kanjis: null,
+    columns: null,
+    cellSize: null,
+    fontSize: null,
+  },
+});
