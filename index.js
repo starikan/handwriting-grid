c01315cbfd5c400273483d8f8a541a659ccde37c
sstarodubov <s.starodubov@litebox.ru>
2019-09-29 11:44:21 +0400

    Print with print.css

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
-const stream = require('stream');
 
 const server = express();
 server.use(express.static(__dirname + '/'));
-
-server.get('/get_pdf', async function (req, res) {
-  // const params = {
-  //   kanjis: '%E6%98%93',
-  //   columns: 7,
-  // }
-  
-  const browser = await puppeteer.launch({
-    defaultViewport: {
-      width: 2000,
-      height: 2000,
-    },
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000/?kanjis=%E6%98%93&columns=6', {
-    "waitUntil": 'networkidle2'
-  }).catch(function () {
-    console.log('Error while loading up the url.');
-  });
-  const selector = await page.$('page[size="A4"]');
-  const screen = await selector.screenshot()
-  browser.close();
-
-  const fileName = '%E6%98%93';
-  var fileContents = Buffer.from(screen, "base64");
-
-  var readStream = new stream.PassThrough();
-  readStream.end(fileContents);
-
-  res.set('Content-disposition', `attachment; filename=${fileName}.png`);
-  res.set('Content-Type', 'text/plain');
-
-  readStream.pipe(res);
-});
 
 server.get('/', async function (req, res) {
   res.sendFile(path.join(public, 'index.html'));
