777e6f616ba6a3c73a6d33895bd512cdd8614ffa
sstarodubov <s.starodubov@litebox.ru>
2019-10-08 10:00:16 +0400

    Grid cells

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ var router = new VueRouter({
   routes: [],
 });
 
+const range = function*(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+};
+
 var app = new Vue({
   router,
   el: '#app',
@@ -15,11 +21,13 @@ var app = new Vue({
           height: false,
           width: false,
         },
-        columnsCount: 0,
-        rowsCount: 0,
+        columnsCount: 7,
+        rowsCount: 3,
         cellBorder: {},
         columnsWidth: {},
         rowsHeight: {},
+        verticalGap: 0.5,
+        horizontalGap: 0.5,
         direction: 'rows',
         symbols: [
           {
@@ -40,6 +48,24 @@ var app = new Vue({
     },
     deletePage: function(index) {
       this.pages.splice(index, 1);
+    },
+    getCellsCount: function(page) {
+      return [...range(0, page.columnsCount * page.rowsCount - 1)];
+    },
+    getCellStyle: function(page, index) {
+      const row = Math.floor(index / page.columnsCount);
+      const col = index - row * page.columnsCount;
+      const styles = {};
+      if (page.direction === 'columns') {
+        styles['grid-column'] = `${row + 1} / ${row + 2}`;
+        styles['grid-row'] = `${col + 1} / ${col + 2}`;
+      } else {
+        styles['grid-row'] = `${row + 1} / ${row + 2}`;
+        styles['grid-column'] = `${col + 1} / ${col + 2}`;
+      }
+
+      const concatStyle = Object.keys(styles).reduce((t, key) => t + `${key}: ${styles[key]};`, '');
+      return concatStyle;
     },
   },
   data: {
