fc49ffb0ad9fdc2e3a96e0ddcefc8ff3f9387e4d
Sergey Starodubov <4analit@gmail.com>
2019-10-30 23:35:29 +0400

    Refactoring

--- src/index.js
+++ src/index.js
@@ -12,48 +12,12 @@ import './index.scss';
 import './print.scss';
 import 'font-awesome/scss/font-awesome.scss';
 
+import { uuidv4, openModal, colsCount } from './js/helpers';
+
 const router = new VueRouter({
   mode: 'history',
   routes: [{ name: 'pages-view', path: '/' }],
 });
-
-const range = function*(start, end) {
-  for (let i = start; i < end; i++) {
-    yield i;
-  }
-};
-
-const uuidv4 = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    let r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
-
-const openModal = function(dialogId, onClose = () => {}) {
-  const modal = document.getElementById(dialogId);
-  modal.showModal();
-
-  const escapeKey = ({ key }) => {
-    key == 'Escape' && modal.close();
-    modal.removeEventListener('keydown', escapeKey);
-  };
-
-  const listnerClick = async event => {
-    event.stopPropagation();
-    if (event.target == modal) {
-      modal.close();
-      modal.removeEventListener('click', listnerClick);
-    }
-  };
-
-  modal.addEventListener('close', onClose);
-  modal.addEventListener('click', listnerClick, false);
-  modal.addEventListener('keydown', escapeKey);
-};
-
-const colsCount = content => content.map(v => v.length).reduce((v, s) => (v > s ? v : s), 0);
 
 const app = new Vue({
   router,
