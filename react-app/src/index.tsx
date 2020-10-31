3ab33a9cb7bd89d2f1f775d04289c61879a79a94
Sergey Starodubov <4analit@gmail.com>
2020-10-31 12:46:54 +0400

    css -> scss

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BlockProps } from './Block/Block';
-import './index.css';
+import './index.scss';
 import Page from './Page';
 
 const App: React.FC = () => {
