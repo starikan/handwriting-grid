4cafe59dadc5ba9fa7e050f9e4a43c3b2e1e3bcc
Sergey Starodubov <4analit@gmail.com>
2023-02-27 22:39:57 +0400

    Refactoring

--- src/components/Header/components/HeaderButtons/HeaderButtons.tsx
+++ src/components/Header/components/HeaderButtons/HeaderButtons.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { IconButton, Tooltip } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
 import CloseIcon from '@mui/icons-material/Close';
 import { selectDocumentById } from '../../../../models/document/document';
 
@@ -11,11 +10,6 @@ export interface HeaderButtonsProps {}
 export function HeaderButtons(props: HeaderButtonsProps) {
   return (
     <div className={styles.HeaderButtons}>
-      <Tooltip title="Settings">
-        <IconButton size="small">
-          <SettingsIcon fontSize="small" />
-        </IconButton>
-      </Tooltip>
       <Tooltip title="Close Document">
         <IconButton size="small" onClick={() => selectDocumentById('')}>
           <CloseIcon fontSize="small" />
